18d051b605133d92ba4ba7a1d98b7894603ea172
irsyadelba <113337346+irsyadelba@users.noreply.github.com>
2022-09-14 12:31:19 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+Class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Irsyad Elba
+            Saya Tinggal di daerah Bekasi
+            Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
+
+
